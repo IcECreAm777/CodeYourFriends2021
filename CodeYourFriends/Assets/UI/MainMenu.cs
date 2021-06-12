69d29dff7df9627fdd3cb81c503539e71beb8518
Booy Lock <booy.l.lock@gmail.com>
2021-06-12 20:32:14 +0200

    MainMenu
    did the main menu and prefab button for ui, also made the play button load the game scene

---
+++ CodeYourFriends/Assets/UI/MainMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Makes the start button load a scene - change the "SampleScene" to the actual game Scene in the future. Also in build settings make sure we ad the scene we want to load.
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
