9f7f15a3358d7d94c5866621d461bf689aae1759
Booy Lock <booy.l.lock@gmail.com>
2021-06-13 18:41:18 +0200

    ui changes

--- CodeYourFriends/Assets/UI/MainMenu.cs
+++ CodeYourFriends/Assets/UI/MainMenu.cs
@@ -8,6 +8,6 @@ public class MainMenu : MonoBehaviour
     //Makes the start button load a scene - change the "SampleScene" to the actual game Scene in the future. Also in build settings make sure we ad the scene we want to load.
     public void StartGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Tutturial dialoge test");
     }
 }
